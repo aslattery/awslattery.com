e6cb9d9523a42de219026f7c5ef9ab0df73add54
Andrew Slattery <andrew@hivemetric.com>
2018-11-11 01:47:24 +0000

    Init default pages

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import DefaultLayout from '../layouts/Default';
+
+const NotFoundPage = () => (
+    <DefaultLayout>
+        <h1>404</h1>
+    </DefaultLayout>
+);
+
+NotFoundPage.propTypes = {};
+
+export default NotFoundPage;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+import DefaultLayout from '../layouts/Default';
+
+const IndexPage = () => (
+    <DefaultLayout />
+);
+
+IndexPage.propTypes = {};
+
+export default IndexPage;
